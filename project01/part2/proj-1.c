fee5779a93e79913d1224e603767c48a0acb1754
Matt Berk <mpberk@asu.edu>
2020-09-16 15:37:02 -0700

    Fixed all compile warnings

--- project01/part2/proj-1.c
+++ project01/part2/proj-1.c
@@ -1,6 +1,54 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "sem.h"
+
+#include "threads.h"
+
+
+
+typedef struct semaphore_t {
+     pthread_mutex_t mutex;
+     pthread_cond_t cond;
+     int count;
+} semaphore_t;
+
+void init_sem(semaphore_t *s, int i)
+{   
+    s->count = i;
+    pthread_mutex_init(&(s->mutex), NULL);
+    pthread_cond_init(&(s->cond), NULL);
+}
+
+
+/*
+ * The P routine decrements the semaphore, and if the value is less than
+ * zero then blocks the process 
+ */
+void P(semaphore_t *sem)
+{   
+    pthread_mutex_lock (&(sem->mutex)); 
+    sem->count--;
+    if (sem->count < 0) pthread_cond_wait(&(sem->cond), &(sem->mutex));
+    pthread_mutex_unlock (&(sem->mutex)); 
+}
+
+
+/*
+ * The V routine increments the semaphore, and if the value is 0 or
+ * negative, wakes up a process and yields
+ */
+
+void V(semaphore_t * sem)
+{   
+    pthread_mutex_lock (&(sem->mutex)); 
+    sem->count++;
+    if (sem->count <= 0) {
+	pthread_cond_signal(&(sem->cond));
+    }
+    pthread_mutex_unlock (&(sem->mutex)); 
+    pthread_yield();
+}
 
 int *x;
 semaphore_t child_done;
@@ -9,7 +57,7 @@ semaphore_t parent_done;
 
 int child (void *arg)
 {
-  int index = (int) arg;
+  int index = *(int*) arg;
   while (1)
     {
       // Increment specific array entry
@@ -31,22 +79,27 @@ int main(int argc, char *argv[])
     {
       count = atoi(argv[1]);
     }
-
-  // Setup array
+  
+  // Setup array for incrementing and indices
+  int *indices;
   x = calloc(count, sizeof(int));
+  indices = calloc(count, sizeof(int));
   for (i = 0; i < count; i++)
+    {
     *(x+i) = 0;
+    *(indices+i) = i;
+    }
 
   // Initialize semaphores
   init_sem(&child_done,0);
   init_sem(&parent_done,0);
-    
+
   // Setup and start pthreads
   pthread_t *pids;
   pids = calloc(count, sizeof(pthread_t));
 
   for (i = 0; i < count; i++)
-    pids[i] = start_thread(child, (void*) i);
+    pids[i] = start_thread(child, (void*) (indices+i));
 
   int failure = 0;
   int j = 0;
