7cedf93586db70918501e6e0f15b8057fa892ab0
Matt Berk <mpberk@asu.edu>
2020-09-16 23:13:41 -0700

    Fixed code to use a semaphore solution similar to the Reader-Writers problem.

--- project01/part2/proj-1.c
+++ project01/part2/proj-1.c
@@ -4,21 +4,71 @@
 #include <stdlib.h>
 #include "sem.h"
 
+int count = 3;
 int *x;
-semaphore_t child_done;
-semaphore_t parent_done;
+int active_children = 0;
+
+semaphore_t mutex; // General mutex for critical sections
+semaphore_t start_children; // Start a child
+semaphore_t start_parent; // Start the parent
+semaphore_t child_wait; // Children waiting for other children to finish up
 
 int child (void *arg)
 {
   int index = *(int*) arg;
   while (1)
     {
+      // Wait for signal to start children
+      P(&start_children);
+
+      // Critical section
+      P(&mutex);
+
+      // Increase number of active children
+      active_children++;
+
+      // Allow other children to start if still more left
+      if (active_children < count)
+        V(&start_children);
+
+      V(&mutex);
+
       // Increment specific array entry
       *(x+index) = *(x+index) + 1;
-      // Signal child is done
-      V(&child_done);
-      // Wait for parent
-      P(&parent_done);
+
+      // Critical section
+      P(&mutex);
+
+      // On last child
+      if (active_children == count)
+        {
+          // Continue all children
+          V(&child_wait);
+          active_children--;
+        }
+
+      V(&mutex);
+
+      // Wait until all children are done
+      P(&child_wait);
+
+      // Critical section
+      P(&mutex);
+
+      // On all but last child
+      if (active_children != 0)
+        {
+          // End another child (violent sounding...)
+          V(&child_wait);
+          active_children--;
+        }
+      else
+        {
+          // On last child, start parent
+          V(&start_parent);
+        }
+
+      V(&mutex);
     }
 }
 
@@ -27,7 +77,6 @@ int main(int argc, char *argv[])
   int i;
 
   // Get number of children from command line
-  int count = 3;
   if (argc >= 2)
     {
       count = atoi(argv[1]);
@@ -43,8 +92,10 @@ int main(int argc, char *argv[])
     }
 
   // Initialize semaphores
-  init_sem(&child_done,0);
-  init_sem(&parent_done,0);
+  init_sem(&mutex,1);
+  init_sem(&start_children,1);
+  init_sem(&start_parent,0);
+  init_sem(&child_wait,0);
 
   // Setup and start pthreads
   pthread_t *pids;
@@ -60,14 +111,15 @@ int main(int argc, char *argv[])
     {
       j = j + 1;
 
-      // Wait for all children
-      for (i = 0; i < count; i++)
-        P(&child_done);
+      // Wait for start_parent from children finishing up
+      P(&start_parent);
 
       // print out array
       for (i = 0; i < count; i++)
         {
           printf("i: %d, x[%d] = %d\n", j, i, *(x+i));
+
+          // On failure of semaphores, break from loop and end program
           if (j != *(x+i))
             failure = 1;
           if ((i == count - 1) && failure)
@@ -77,10 +129,7 @@ int main(int argc, char *argv[])
       if (failure)
         break;
 
-      // Signal to children that parent is done
-      for (i = 0; i < count; i++)
-        V(&parent_done);
-
+      // Start children
+      V(&start_children);
     }
-
 }
