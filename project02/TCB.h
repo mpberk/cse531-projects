f986a1db770a6823ea3e6566f6169d2c620ce7af
Matt Berk <mpberk@asu.edu>
2020-10-01 16:30:33 -0700

    Added ability to pass arguments to threads

--- project02/TCB.h
+++ project02/TCB.h
@@ -10,7 +10,7 @@ typedef struct TCB_t
   ucontext_t context;
 } TCB_t;
 
-void init_TCB (TCB_t *tcb, void *function, void *stackP, int stack_size)
+void init_TCB (TCB_t *tcb, void *function, void *args, void *stackP, int stack_size)
 // arguments to init_TCB are
 //   1. pointer to the function, to be executed
 //   2. pointer to the thread stack
@@ -20,7 +20,7 @@ void init_TCB (TCB_t *tcb, void *function, void *stackP, int stack_size)
   getcontext(&tcb->context); // have to get parent context, else snow forms on hell
   tcb->context.uc_stack.ss_sp = stackP;
   tcb->context.uc_stack.ss_size = (size_t) stack_size;
-  makecontext(&tcb->context, function, 0); // context is now cooked
+  makecontext(&tcb->context, function, 1, args); // context is now cooked
 }
 
 #endif
