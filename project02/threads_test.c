5b49b66527769992a4f7cc38e0d25f67a6e94088
Matt Berk <mpberk@asu.edu>
2020-10-01 16:33:32 -0700

    Added stimulus based on project01's thread_test.c

--- project02/threads_test.c
+++ project02/threads_test.c
@@ -1,9 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "threads.h"
 
+// Number of children functions to run
+int count = 3;
+
+// Global variable
+int global = 0;
+
+// Easily modifiable function for sleep
+void go_to_sleep()
+{
+  sleep(1);
+}
+
+// Child function to have multiple copies running
+void function (void *arg)
+{
+  int value = *(int*) arg;
+  int local = 0;
+
+  while (1)
+    {
+      printf("Function %02d: global = %d, local = %d\n", value, global, local);
+      go_to_sleep();
+      global++;
+      local++;
+      go_to_sleep();
+      printf("Function %02d: incremented, global = %d, local = %d\n", value, global, local);
+      go_to_sleep();
+      yield();
+    }
+}
+
 int main (int argc, char *argv[])
 {
-  return 0;
+  int i; // for loop variable
+  
+  // Get number of children from command line
+  if (argc >= 2)
+    {
+      count = atoi(argv[1]);
+    }
+
+  // Setup array for indices
+  int *indices;
+  indices = (int*) malloc(count * sizeof(int));
+  for (i = 0; i < count; i++)
+    {
+      *(indices+i) = i;
+    };
+
+  // Setup different threads
+  for (i = 0; i < count; i++)
+    {
+      start_thread(function, (void*) (indices+i));
+    };
+
+  run();
+  
 };
