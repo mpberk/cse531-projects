1c0072f4ae5332d7074ac954dadfc23dfdf2e7ba
Hongquy Le Nguyen <hongquy1999@gmail.com>
2020-10-06 00:43:26 -0700

    Add files via upload

--- project02/hongquy/q.h
+++ project02/hongquy/q.h
@@ -9,29 +9,32 @@ TCB_t* NewItem(){
 	TCB_t* newPtr = (TCB_t*) malloc(sizeof(TCB_t));
 	newPtr->next = NULL;
 	newPtr->prev = NULL;
-	
+//	newPtr->thread_id = NULL;
+
 	getcontext(&(newPtr->context));
-//	newPtr->thread_id = -1;
+	
 	return newPtr;
 }
 
 TCB_t** newQueue(){
 	TCB_t** head = (TCB_t**) malloc(sizeof(TCB_t*));
-	(*head) = NewItem();
 	return head;
 }
 
 void AddQueue(TCB_t** head, TCB_t* item){
-	if(*head == NULL){
+	if(head == NULL){
 		printf("error, queue not made\n");
 		exit(1);
 	}
+	else if(*head == NULL ){
+		*head = item;
+	}	
 	else if((*head)->next == NULL){
 		(*head)->next = item;
 		(*head)->prev = item;
 		item->prev = (*head);
 		item->next = (*head);
-		printf("Item added! 1\n");
+//		printf("Item added! 1\n");
 	}
 	else if((*head)->next != NULL){
 		TCB_t* tail = (*head)->prev;
@@ -40,7 +43,7 @@ void AddQueue(TCB_t** head, TCB_t* item){
 		tail->next->next = (*head);
 		tail->next->prev = tail;
 		(*head)->prev = tail->next;
-		printf("Item added! 2\n");
+//		printf("Item added! 2\n");
 	}
 }
 
