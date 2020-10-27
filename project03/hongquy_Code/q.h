999ccb2d07ac4ff61e0cf017c8dbfa60e0fc64ce
Hongquy Le Nguyen <hongquy1999@gmail.com>
2020-10-26 19:31:38 -0700

    Add files via upload

--- project03/hongquy_Code/q.h
+++ project03/hongquy_Code/q.h
@@ -27,15 +27,23 @@ void AddQueue(TCB_t** head, TCB_t* item){
 		exit(1);
 	}
 	else if(*head == NULL ){
-		item->prev = item;
-		item->next = item;
 		*head = item;
 	}	
-	else{
-		item->next = (*head);
-		item->prev = (*head)->prev;
-		(*head)->prev->next = item;
+	else if((*head)->next == NULL){
+		(*head)->next = item;
 		(*head)->prev = item;
+		item->prev = (*head);
+		item->next = (*head);
+//		printf("Item added! 1\n");
+	}
+	else if((*head)->next != NULL){
+		TCB_t* tail = (*head)->prev;
+
+		tail->next = item;
+		tail->next->next = (*head);
+		tail->next->prev = tail;
+		(*head)->prev = tail->next;
+//		printf("Item added! 2\n");
 	}
 }
 
@@ -43,18 +51,32 @@ TCB_t* DelQueue(TCB_t** head){
 
 	TCB_t* delEle = NULL;
 	
-	if(*head == NULL){
+	if((*head) == NULL){
 		printf("error, queue empty\n");
 		exit(1);
 	}
-	else{
+	else if((*head)->next == NULL){
 		delEle = *head;
-		(*head)->next->prev = (*head)->prev;
-		(*head)->prev->next = (*head)->next;
-		*head = (*head)->next;
-		delEle->next = NULL;
-		delEle->prev = NULL;
+		(*head) = NULL;
+	///	printf("Item deleted 1\n");
 	}
+	else if((*head)->next != NULL && (*head)->next != (*head)){
+		delEle = *head;
+		TCB_t* tail = (*head)->prev;
+		
+		tail->next = (*head)->next;
+		(*head)->next->prev = tail;
+
+		(*head) = (*head)->next;
+	//	printf("Item deleted 2\n");
+	
+	}
+	else if((*head)->next !=NULL && (*head)->next == *head){
+		delEle = *head;
+		*head = NULL;
+	//	printf("Item deleted 3\n");
+	}
+
 	return delEle;
 	
 }
